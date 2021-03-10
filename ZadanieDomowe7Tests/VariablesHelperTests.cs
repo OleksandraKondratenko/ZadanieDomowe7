266aba98777cea1c5274128d61061d38abde1f9f
OleksandraKondratenko <oleksandrakondratenko@gmail.com>
2021-03-10 17:26:00 +0200

    Change Name in VariableHelperTest Class

--- ZadanieDomowe7Tests/VariablesHelperTests.cs
+++ ZadanieDomowe7Tests/VariablesHelperTests.cs
@@ -8,7 +8,7 @@ namespace ZadanieDomowe7Tests
     public class VariablesHelperTests
     {
         [TestCase(3, 2, -19)]
-        public void WhenEquationIsSolved(int num1, int num2, double expected)
+        public void GetEquationValue_WhenEquationIsSolved_ShouldReturnAnswer(int num1, int num2, double expected)
         {
             double result = VariablesHelper.GetEquationValue(num1, num2);
             Assert.AreEqual(expected, result);
@@ -16,14 +16,14 @@ namespace ZadanieDomowe7Tests
 
         [TestCase(3,3)]
 
-        public void WhenEquationGenerateException(int num1, int num2)
+        public void GetEquationValue_WhenEquationIsNotSolved_ShouldReturnException(int num1, int num2)
         {
             Assert.Throws<DivideByZeroException>(() => VariablesHelper.GetEquationValue(num1, num2));
         }
 
         [TestCase(3, 4, 4, 3)]
         [TestCase(8, 5, 5, 8)]
-        public void SwapValuesTest(int num1, int num2, int expectedNum1, int expectedNum2)
+        public void Swap_WhenTestIsPassed_ShouldChangeNumbers(int num1, int num2, int expectedNum1, int expectedNum2)
         {
             VariablesHelper.Swap(ref num1, ref num2);
 
@@ -34,15 +34,15 @@ namespace ZadanieDomowe7Tests
             Assert.AreEqual(actualNum2, expectedNum2);
         }
 
-        [Test, TestCaseSource("DivideCases")]
-        public void WhenDivisionAndDivisionReminderIsPassed(int num1, int num2, (int, int) expected)
+        [Test, TestCaseSource(nameof(DataDivideCases))]
+        public void GetDivisionAndDivisionReminder_WhenTestIsPassed_ShouldBeCalculated(int num1, int num2, (int, int) expected)
         {
             (int, int) result = VariablesHelper.GetDivisionAndDivisionReminder(num1, num2);
            
             Assert.AreEqual(expected, result);
         }
 
-        static IEnumerable<object[]> DivideCases ()
+        static IEnumerable<object[]> DataDivideCases ()
         {
             yield return new object[] { 10, 3, (3, 1) };
         }
@@ -50,25 +50,25 @@ namespace ZadanieDomowe7Tests
         [TestCase(5,0)]
         [TestCase(10,0)]
 
-        public void WhenDivisionAndDivisionReminderGenerateException(int num1, int num2)
+        public void GetDivisionAndDivisionReminder_WhenTestIsNotPassed_ShouldReturnException(int num1, int num2)
         {
             Assert.Throws<DivideByZeroException>(() => VariablesHelper.GetDivisionAndDivisionReminder(num1,num2));
         }
 
         [TestCase(2,6,20,7)]
-        public void WhenLinearEquationValueIsPassed(int num1, int num2, int num3, double expected)
+        public void GetLinearEquationValue_WhenTestIsPassed_ShouldBeCalculate(int num1, int num2, int num3, double expected)
         {
             double result = VariablesHelper.GetLinearEquationValue(num1, num2, num3);
             Assert.AreEqual(expected, result);
         }
         
         [TestCase(0,5,6)]
-         public void WhenLinearEquationValueGenereteException(int num1, int num2, int num3)
+         public void GetLinearEquationValue_WhenTestIsNotPassed_ShouldReturnException(int num1, int num2, int num3)
         {
             Assert.Throws<DivideByZeroException>(() => VariablesHelper.GetLinearEquationValue(num1, num2, num3));
         }
 
-        [TestCaseSource("DataForLineEquation")]
+        [TestCaseSource(nameof(DataForLineEquation))]
         public void WhenEquationOfTheLineIsPassed(int x1, int y1, int x2, int y2, (double, double)  expected)
         {
             (double, double) result = VariablesHelper.GetEquationOfTheLine(x1, y1, x2, y2);
